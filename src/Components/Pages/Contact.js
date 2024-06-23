54492a14299754d78471f93bd76f737ecde70b98
Paul Them <->
2024-06-23 18:45:47 -0400

    fixing contact form by adding name fields. Modifying form styles. deleting unneeded contact components

--- src/Components/Pages/Contact.js
+++ src/Components/Pages/Contact.js
@@ -1,34 +1,161 @@
-import React from 'react';
-import "../ContactForm/Contact.css";
+import React, { useEffect, useState } from "react";
+import { Form, Button, FormGroup, Label, Input } from "reactstrap";
+import "./Contact.css";
 
 export default function Contact() {
+
+    let [firstName, setFirst] = useState(getInitialFirst());
+    let [lastName, setLast] = useState(getInitialLast());
+    let [email, setEmail] = useState(getInitialEmail());
+    let [message, setMessage] = useState(getInitialMessage());
+
+    useEffect(saveFirstName, [firstName]);
+    useEffect(saveLastName, [lastName]);
+    useEffect(saveEmail, [email]);
+    useEffect(saveMessage, [message]);
+
+    function saveFirstName() {
+        localStorage.setItem("first", [firstName]);
+    }
+
+    function saveLastName() {
+        localStorage.setItem("last", [lastName]);
+    }
+
+    function saveEmail() {
+        localStorage.setItem("emailEntered", [email]);
+    }
+
+    function saveMessage() {
+        localStorage.setItem("messageEntered", [message])
+    }
+
+
+    function getInitialFirst() {
+        let savedState = localStorage.getItem("first");
+        if (typeof savedState === "string") {
+            return savedState;
+        }
+        return "";
+    }
+
+    function getInitialLast() {
+        let savedState = localStorage.getItem("last");
+        if (typeof savedState === "string") {
+            return savedState;
+        }
+        return "";
+    }
+
+    function getInitialEmail() {
+        let savedState = localStorage.getItem("emailEntered");
+        if (typeof savedState === "string") {
+            return savedState;
+        }
+        return "";
+    }
+
+    function getInitialMessage() {
+        let savedState = localStorage.getItem("messageEntered");
+        if (typeof savedState === "string") {
+            return savedState;
+        }
+        return "";
+    }
+
+    function handleFirstNameChange(e) {
+        setFirst(e.target.value);
+    }
+
+    function handleLastNameChange(e) {
+        setLast(e.target.value);
+    }    
+    
+    function handleEmailChange(e) {
+        setEmail(e.target.value);
+    }
+
+    function handleMessageChange(e) {
+        setMessage(e.target.value);
+    }
+
   return (
-<section class="page-start">
+    <section class="page-start">
 
-<h1>Contact us today!</h1>
-      
-<form action="https://formspree.io/f/mgegpeov" method="post" class="col-12 offset-md-2 col-md-8 offset-xl-3 col-xl-6">
-        <div>
-            <label for="first-name-area" class="form-label">First name:</label>
-            <input type="text" class="form-control" id="first-name-area" name="First Name" required=""/>
-        </div>
-        <div>
-            <label for="last-name-area" class="form-label">Last name:</label>
-            <input type="text" class="form-control" id="last-name-area" name="Last Name" required=""/>
-        </div>
+    <h1>Contact us today!</h1>
+    
+    <Form action="https://formspree.io/f/mgegpeov" method="post" class="col-12 offset-md-2 col-md-8 offset-xl-3 col-xl-6">
+        <FormGroup className="contact-row">
+            <Label
+                htmlFor="first-name-area"
+            >
+                First Name
+            </Label>
+            <Input
+                type="text"
+                id="first-name-area"
+                name="First Name"
+                onChange={handleFirstNameChange}
+                value={firstName}
+                required
+            >
+            </Input>
+        </FormGroup>
+        <FormGroup className="contact-row">
+            <Label
+                htmlFor="last-name-area"
+            >
+                Last Name
+            </Label>
+            <Input
+                type="text"
+                id="last-name-area"
+                name="Last Name"
+                onChange={handleLastNameChange}
+                value={lastName}
+                required
+            >
+            </Input>
+        </FormGroup>
+        <FormGroup className="contact-row">
+            <Label
+                htmlFor="email-area"
+            >
+                Email
+            </Label>
+            <Input
+                type="email"
+                id="email-area"
+                name="Email"
+                onChange={handleEmailChange}
+                value={email}
+                required
+            >
+            </Input>
+        </FormGroup>
+        <FormGroup className="contact-row">
+            <Label
+                htmlFor="message-area"
+            >
+                Message
+            </Label>
+            <textarea
+                type="text"
+                name="message-area"
+                id="message-area"
+                rows="4"
+                onChange={handleMessageChange}
+                value={message}
+                required
+            >
+            </textarea>
+        </FormGroup>
 
-    <div>
-        <label for="email-field" class="form-label">Email address:</label>
-        <input type="email" class="form-control" id="email-field" name="Email" required=""/>
-    </div>
+        <Button className="contact-btn" type="submit">
+            Submit
+        </Button>
+    </Form>
 
-    <div>
-        <label for="message-area" class="form-label">Message:</label>
-        <textarea name="message-area" class="form-control" id="message-area" rows="4" required=""></textarea>
-    </div>
-
-    <button type="submit" class="form-control contact-btn ">Submit</button>
-</form>
-</section>
+    </section>
   );
 }
